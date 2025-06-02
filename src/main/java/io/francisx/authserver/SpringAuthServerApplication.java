8351525cb967347b2f63b021355c6119408530dc
fr4ncisx <fr4ncisx@gmail.com>
2025-06-02 14:49:29 -0300

    Added FeignClient annotation

--- src/main/java/io/francisx/authserver/SpringAuthServerApplication.java
+++ src/main/java/io/francisx/authserver/SpringAuthServerApplication.java
@@ -2,7 +2,9 @@ package io.francisx.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringAuthServerApplication {
 	public static void main(String[] args) {
